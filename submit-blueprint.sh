d423485a94fc227e317a9a53f1ed1329176c544e
Randy Gelhausen <rgelhau@gmail.com>
2016-09-16 00:13:31 -0400

    fixed submit blueprint and added yarn port to single container

--- submit-blueprint.sh
+++ submit-blueprint.sh
@@ -1,6 +1,6 @@
 TYPE=$1
 BLUEPRINT=$2
-source .env
+source ./.env
 
 curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/blueprints/$TYPE --data-binary "@$BLUEPRINT"
 curl --user admin:admin -H 'X-Requested-By:admin' -X PUT $AMBARI_HOST/api/v1/stacks/HDP/versions/$HDP_VERSION/operating_systems/$OS/repositories/HDP-${HDP_VERSION} -d '{"Repositories":{"base_url":"'$BASE_URL'", "verify_base_url":true}}'
