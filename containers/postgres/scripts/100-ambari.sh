9ae8f7c0bfa19ad130c49f7a7320fe9afb93fd7f
sarath-mec <sarath.mec@gmail.com>
2017-05-07 15:45:12 +0530

    Change 1

--- containers/postgres/scripts/100-ambari.sh
+++ containers/postgres/scripts/100-ambari.sh
@@ -2,8 +2,8 @@
 set -e
 
 cd ~/
-wget https://raw.githubusercontent.com/apache/ambari/branch-2.5/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
-sed -i "s/\${ambariSchemaVersion}/2.5.0/g" Ambari-DDL-Postgres-CREATE.sql
+wget $AMBARI_DDL_URL_MYSQL
+sed -i "s/\${ambariSchemaVersion}/2.5.0/g" Ambari-DDL-MySQL-CREATE.sql
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     create database ambari;
