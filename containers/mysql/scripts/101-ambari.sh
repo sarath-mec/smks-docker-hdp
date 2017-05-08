70796b2671c39c61961b63a1ce4a3ba31b01f37a
sarath-mec <sarath.mec@gmail.com>
2017-05-08 16:27:47 +0530

    Update 101-ambari.sh

--- containers/mysql/scripts/101-ambari.sh
+++ containers/mysql/scripts/101-ambari.sh
@@ -2,7 +2,7 @@
 set -e
 
 cd ~/
-wget $(AMBARI_DDL_URL_MYSQL)
+wget $AMBARI_DDL_URL_MYSQL
 sed -i "s/\${ambariSchemaVersion}/2.5.0/g" Ambari-DDL-MySQL-CREATE.sql
 
-mysql < /Ambari-DDL-MySQL-CREATE.sql
+mysql < Ambari-DDL-MySQL-CREATE.sql
