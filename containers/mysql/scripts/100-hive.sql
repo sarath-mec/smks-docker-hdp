262e3e6e6a13a108a9bd39740976014df136ada9
sarath-mec <sarath.mec@gmail.com>
2017-05-08 09:18:34 +0530

    Update 100-hive.sql

--- containers/mysql/scripts/100-hive.sql
+++ containers/mysql/scripts/100-hive.sql
@@ -1,3 +1,15 @@
-create user hive with password 'dev';
-create database hive;
-alter database hive owner to hive;
+create user 'ambari'@'%' identified by 'dev';
+grant all privileges on *.* to 'ambari'@'%'; 
+create user 'ambari'@'localhost' identified by 'dev';
+grant all privileges on *.* to 'ambari'@'localhost';
+create user'ambari'@'ambari-server.dev' identified by 'dev';
+grant all privileges on *.* to 'ambari'@'ambari-server.dev';
+flush privileges;
+
+create user 'hive'@'%' identified by 'dev';
+grant all privileges on *.* to 'hive'@'%'; 
+create user 'hive'@'localhost' identified by 'dev';
+grant all privileges on *.* to 'hive'@'localhost';
+create user'hive'@'dn0.dev' identified by 'dev';
+grant all privileges on *.* to 'hive'@'dn0.dev';
+flush privileges;
