5d3967c5fa33002250faa4f55db57dd5459f1969
sarath-mec <sarath.mec@gmail.com>
2017-05-08 09:22:51 +0530

    Update 100-ambari.sh

--- containers/mysql/scripts/100-ambari.sh
+++ containers/mysql/scripts/100-ambari.sh
@@ -5,19 +5,7 @@ cd ~/
 wget https://raw.githubusercontent.com/apache/ambari/branch-2.5/ambari-server/src/main/resources/Ambari-DDL-Postgres-CREATE.sql
 sed -i "s/\${ambariSchemaVersion}/2.5.0/g" Ambari-DDL-Postgres-CREATE.sql
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    create database ambari;
-    create user ambari with password 'dev';
-    GRANT ALL PRIVILEGES ON DATABASE ambari TO ambari;
+mysql --user=root --password=admin
 
-    CREATE SCHEMA ambari AUTHORIZATION ambari;
-    ALTER SCHEMA ambari OWNER TO ambari;
-    ALTER SCHEMA public OWNER to ambari;
+--port 3306
 
-    ALTER ROLE ambari SET search_path to 'ambari', 'public';
-
-    \connect ambari ambari;
-    \i Ambari-DDL-Postgres-CREATE.sql 
-EOSQL
-
-cp /pg_hba.conf /var/lib/postgresql/data/
