e9252f81bc25722a6ebf278272eee57270515945
sarath-mec <sarath.mec@gmail.com>
2017-05-08 15:33:00 +0530

    Update 100-create_db.sql

--- containers/mysql/scripts/100-create_db.sql
+++ containers/mysql/scripts/100-create_db.sql
@@ -1,17 +1,35 @@
 create user 'ambari'@'%' identified by 'dev';
-grant all privileges on *.* to 'ambari'@'%'; 
+grant all privileges on *.* to 'ambari'@'%';
 create user 'ambari'@'localhost' identified by 'dev';
 grant all privileges on *.* to 'ambari'@'localhost';
 flush privileges;
 
 create user 'hive'@'%' identified by 'dev';
-grant all privileges on *.* to 'hive'@'%'; 
+grant all privileges on *.* to 'hive'@'%';
 create user 'hive'@'localhost' identified by 'dev';
 grant all privileges on *.* to 'hive'@'localhost';
 flush privileges;
 
 create user 'oozie'@'%' identified by 'dev';
-grant all privileges on *.* to 'oozie'@'%'; 
+grant all privileges on *.* to 'oozie'@'%';
 create user 'oozie'@'localhost' identified by 'dev';
 grant all privileges on *.* to 'oozie'@'localhost';
+flush privileges;
+
+create user 'ranger'@'%' identified by 'dev';
+grant all privileges on *.* to 'ranger'@'%';
+create user 'ranger'@'localhost' identified by 'dev';
+grant all privileges on *.* to 'ranger'@'localhost';
+flush privileges;
+
+create user 'rangeradmin'@'%' identified by 'dev';
+grant all privileges on *.* to 'rangeradmin'@'%';
+create user 'rangeradmin'@'localhost' identified by 'dev';
+grant all privileges on *.* to 'rangeradmin'@'localhost';
+flush privileges;
+
+create user 'rangerkms'@'%' identified by 'dev';
+grant all privileges on *.* to 'rangerkms'@'%';
+create user 'rangerkms'@'localhost' identified by 'dev';
+grant all privileges on *.* to 'rangerkms'@'localhost';
 flush privileges;
