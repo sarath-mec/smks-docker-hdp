3eb74f1fdcacb5c1992df3d38c588a56ae660ed0
sarath-mec <sarath.mec@gmail.com>
2017-05-08 18:18:05 +0530

    Update 100-setup_db.sql

--- containers/mysql/scripts/100-setup_db.sql
+++ containers/mysql/scripts/100-setup_db.sql
@@ -1,39 +1,40 @@
+create database ambari;
 create user 'ambari'@'%' identified by 'dev';
-grant all privileges on *.* to 'ambari'@'%';
+grant all privileges on ambari.* to 'ambari'@'%';
 create user 'ambari'@'localhost' identified by 'dev';
-grant all privileges on *.* to 'ambari'@'localhost';
+grant all privileges on ambari.* to 'ambari'@'localhost';
 flush privileges;
 
+create database hive;
 create user 'hive'@'%' identified by 'dev';
-grant all privileges on *.* to 'hive'@'%';
+grant all privileges on hive.* to 'hive'@'%';
 create user 'hive'@'localhost' identified by 'dev';
-grant all privileges on *.* to 'hive'@'localhost';
+grant all privileges on hive.* to 'hive'@'localhost';
 flush privileges;
 
+create database oozie;
 create user 'oozie'@'%' identified by 'dev';
-grant all privileges on *.* to 'oozie'@'%';
+grant all privileges on oozie.* to 'oozie'@'%';
 create user 'oozie'@'localhost' identified by 'dev';
-grant all privileges on *.* to 'oozie'@'localhost';
+grant all privileges on oozie.* to 'oozie'@'localhost';
 flush privileges;
 
 create user 'ranger'@'%' identified by 'dev';
-grant all privileges on *.* to 'ranger'@'%';
 create user 'ranger'@'localhost' identified by 'dev';
-grant all privileges on *.* to 'ranger'@'localhost';
-flush privileges;
 
+create database ranger;
 create user 'rangeradmin'@'%' identified by 'dev';
-grant all privileges on *.* to 'rangeradmin'@'%';
+grant all privileges on ranger.* to 'rangeradmin'@'%';
 create user 'rangeradmin'@'localhost' identified by 'dev';
-grant all privileges on *.* to 'rangeradmin'@'localhost';
+grant all privileges on ranger.* to 'rangeradmin'@'localhost';
 flush privileges;
 
+create database rangerkms;
 create user 'rangerkms'@'%' identified by 'dev';
-grant all privileges on *.* to 'rangerkms'@'%';
+grant all privileges on rangerkms.* to 'rangerkms'@'%';
 create user 'rangerkms'@'localhost' identified by 'dev';
-grant all privileges on *.* to 'rangerkms'@'localhost';
+grant all privileges on rangerkms*.* to 'rangerkms'@'localhost';
 flush privileges;
 
-create database hdp_db;
-use hdp_db;
+use ambari;
 source /mysql/Ambari-DDL-MySQL-CREATE.sql;
