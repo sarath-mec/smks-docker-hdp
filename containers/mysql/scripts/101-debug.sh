5c1b8842b1798ea678709d70f205e93ea6fe744b
sarath-mec <sarath.mec@gmail.com>
2017-05-08 18:42:45 +0530

    Update 101-debug.sh

--- containers/mysql/scripts/101-debug.sh
+++ containers/mysql/scripts/101-debug.sh
@@ -4,7 +4,6 @@ set -e
 #cd /mysql
 #wget $AMBARI_DDL_URL_MYSQL
 #sed -i "s/\${ambariSchemaVersion}/2.5.0/g" Ambari-DDL-MySQL-CREATE.sql
-
 #mysql --user=ambari --password=dev < Ambari-DDL-MySQL-CREATE.sql
 whoami
 echo $MYSQL_ROOT_PASSWORD
