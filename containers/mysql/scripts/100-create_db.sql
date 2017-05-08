2e542d7a5507cd01d6645fd42df7c08b4d0475db
sarath-mec <sarath.mec@gmail.com>
2017-05-08 15:31:08 +0530

    Update and rename 100-hive.sql to 100-create_db.sql

--- containers/mysql/scripts/100-create_db.sql
+++ containers/mysql/scripts/100-create_db.sql
@@ -2,14 +2,16 @@ create user 'ambari'@'%' identified by 'dev';
 grant all privileges on *.* to 'ambari'@'%'; 
 create user 'ambari'@'localhost' identified by 'dev';
 grant all privileges on *.* to 'ambari'@'localhost';
-create user'ambari'@'ambari-server.dev' identified by 'dev';
-grant all privileges on *.* to 'ambari'@'ambari-server.dev';
 flush privileges;
 
 create user 'hive'@'%' identified by 'dev';
 grant all privileges on *.* to 'hive'@'%'; 
 create user 'hive'@'localhost' identified by 'dev';
 grant all privileges on *.* to 'hive'@'localhost';
-create user'hive'@'dn0.dev' identified by 'dev';
-grant all privileges on *.* to 'hive'@'dn0.dev';
+flush privileges;
+
+create user 'oozie'@'%' identified by 'dev';
+grant all privileges on *.* to 'oozie'@'%'; 
+create user 'oozie'@'localhost' identified by 'dev';
+grant all privileges on *.* to 'oozie'@'localhost';
 flush privileges;
